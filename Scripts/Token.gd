fb6c626db1468d3fed87bfa209bdee58982b40c9
kennethbchen <kennethbchen@gmail.com>
2023-03-09 20:44:55 -0500

    Instead of cylindershape, approximating with boxshape

--- Scripts/Token.gd
+++ Scripts/Token.gd
@@ -10,21 +10,20 @@ extends RigidBody3D
 @export_range(0.001, 10, 0.001, "or_greater") var height: float = 0.2
 @export_range(0.001, 10, 0.001, "or_greater") var radius: float = 0.5
 
-var cylinder_shape: CylinderShape3D
+var cylinder_collider: BoxShape3D
 var cylinder_mesh: CylinderMesh
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	assert(collision.shape is CylinderShape3D)
+	assert(collision.shape is BoxShape3D)
 	assert(mesh.mesh is CylinderMesh)
 	
-	cylinder_shape = collision.shape as CylinderShape3D
+	cylinder_collider = collision.shape as BoxShape3D
 	cylinder_mesh = mesh.mesh as CylinderMesh
 	
 	# Change dimensions of token
-	cylinder_shape.radius = radius
-	cylinder_shape.height = height
-	
+	cylinder_collider.size = Vector3(radius * sqrt(2), height, radius * sqrt(2))
+
 	cylinder_mesh.top_radius = radius
 	cylinder_mesh.bottom_radius = radius
 	cylinder_mesh.height = height
@@ -41,9 +40,8 @@ func _process(delta):
 	
 	
 	if Engine.is_editor_hint():
-		cylinder_shape.radius = radius
-		cylinder_shape.height = height
-	
+		cylinder_collider.size = Vector3(radius * sqrt(2), height, radius * sqrt(2))
+
 		cylinder_mesh.top_radius = radius
 		cylinder_mesh.bottom_radius = radius
 		cylinder_mesh.height = height
